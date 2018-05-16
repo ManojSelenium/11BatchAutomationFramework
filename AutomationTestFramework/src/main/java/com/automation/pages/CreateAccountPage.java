23dc9e7c5d86e69ace5303214e1bacb650997b77
ManojSelenium <manojqa5006@gmail.com>
2018-05-16 08:22:35 +0530

    Added Changes

--- AutomationTestFramework/src/main/java/com/automation/pages/CreateAccountPage.java
+++ AutomationTestFramework/src/main/java/com/automation/pages/CreateAccountPage.java
@@ -9,6 +9,7 @@ import com.automation.util.WebDriverUtils;
 public class CreateAccountPage extends WebDriverUtils implements CreateAccountPageProperties{
 
 	WebDriver driver;
+	
 	public CreateAccountPage(WebDriver driver) {
 		super(driver);
 		this.driver=driver;
